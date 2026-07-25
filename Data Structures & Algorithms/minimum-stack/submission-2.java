baf6dd7aaea0baa1c661482a51aae1ea4c713223
Karan Rohra <64662748+KaranRohra@users.noreply.github.com>
2026-07-25 14:25:41 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,34 @@
+class MinStack {
+    Map<Integer, Integer> map;
+    Deque<Integer> stack;
+    Queue<Integer> minQue;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        map = new HashMap<>();
+        minQue = new PriorityQueue<>();
+    }
+
+    public void push(int val) {
+        map.put(val, map.getOrDefault(val, 0) + 1);
+        stack.push(val);
+        minQue.add(val);
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        int cnt = map.get(val);
+        map.put(val, cnt - 1);
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        while (map.get(minQue.peek()) == 0) {
+            minQue.poll();
+        }
+        return minQue.peek();
+    }
+}
